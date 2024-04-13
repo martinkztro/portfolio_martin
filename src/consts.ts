76f02cc295f7e50430a0531786ba7991bd577442
martinkztro <mcastro_20@alu.uabcs.mx>
2024-04-12 19:33:42 -0700

    Initial Commit

---
+++ src/consts.ts
@@ -0,0 +1,28 @@
+import type { Site, Links} from "@types"
+
+// Global
+export const SITE: Site = {
+  TITLE: "",
+  DESCRIPTION: "Welcome to my portfolio. ",
+}
+
+
+// Links
+export const LINKS: Links = [
+  { 
+    TEXT: "About Me", 
+    HREF: "#aboutme", 
+  },
+  { 
+    TEXT: "Projects", 
+    HREF: "#projects", 
+  },
+  { 
+    TEXT: "Skillset", 
+    HREF: "#skillset", 
+  },
+  { 
+    TEXT: "Contact", 
+    HREF: "#contact", 
+  },
+]
---
+++ src/lib/utils.ts
@@ -0,0 +1,14 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatDate(date: Date) {
+  return Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "2-digit",
+    year: "numeric"
+  }).format(date)
+}
